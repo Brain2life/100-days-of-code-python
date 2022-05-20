6a6f975832fcf39a18fd108bee1875e3eba64738
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2022-05-21 00:03:41 +0600

    [file]: Add brand-name-generator-start folder

---
+++ brand-name-generator-start/main.py
@@ -0,0 +1,13 @@
+# https://replit.com/@appbrewery/band-name-generator-start#main.py
+
+#1. Create a greeting for your program.
+print("Hello, the wanderer!\n")
+
+#2. Ask the user for the city that they grew up in.
+city = input("In what city you grew up? \n")
+
+#3. Ask the user for the name of a pet.
+pet_name = input("What is the name of your pet? \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name is: \n", city + " " + pet_name)
