f4965f57a4a1190280a17995ced864db424624b8
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2022-05-23 22:30:54 +0600

    [file]: Add exercise-1

---
+++ exercise-1/exercise-1.py
@@ -0,0 +1,8 @@
+# https://replit.com/@appbrewery/day-2-1-exercise?v=1#main.py
+
+two_digit_number = input("Type a two digit number: ")
+
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+
+print(first_number + second_number)
