bbed059546feabb25e75df26d78243a8b5fc6c58
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2022-05-28 21:36:39 +0600

    [feat]: Add .format() for double precision

--- tip-calculator/tip_calculator.py
+++ tip-calculator/tip_calculator.py
@@ -6,10 +6,13 @@
 # Number of people: 7
 # Output: $19.93
 
+# To avoid issues with zero number precision use .format() function
+
 print("Welcome to the tip calculator.")
 total_bill = float(input("What was the total bill? $"))
 percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 number_of_people = int(input("How many people to split the bill? "))
 
 each_person_payment = round((total_bill / number_of_people) * (1 + percentage / 100), 2)
-print(f"Each person should pay: ${each_person_payment}")
+final_amount = "{:.2f}".format(each_person_payment)
+print(f"Each person should pay: ${final_amount}")
