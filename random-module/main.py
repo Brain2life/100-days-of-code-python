f14bf2b269927772f4a4fc84b53362711e3d1b27
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2022-06-08 18:40:02 +0600

    [file]: Add random-module

---
+++ random-module/main.py
@@ -0,0 +1,16 @@
+import random
+# import my_module
+
+# Create random integer in the given range
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+# Create random float numbers in the interval [0, 1)
+random_float = random.random()
+print(random_float)
+
+# Create random decimal numbers in range [0, 5)
+random_decimal = random_float * 5
+print(random_decimal)
+
+# print(my_module.pi)
